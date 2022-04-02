93b428ea03427c2eddb5cc870bf691d68c7b31ce
Archange <archangabiran@gmail.com>
2022-04-02 23:17:36 +0200

    new add

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,41 +25,74 @@ class MyApp extends StatelessWidget {
                       icon: const Icon(Icons.search),
                       onPressed: () {})
                 ]),
-            body: Container(
-                decoration: const BoxDecoration(color: Colors.yellowAccent),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Hello Mr Archange',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
-                            fontSize: 15)),
-                    const Text('Hello Mr Archange',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
-                            fontSize: 15)),
-                    const Text('Hello Mr Archange',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
-                            fontSize: 15)),
-                    const Expanded(
-                      child: Text('Hello Mr Archange',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontStyle: FontStyle.italic,
-                              fontSize: 15)),
-                    ),
-                    Container(
-                        decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 104, 81, 13),
-                            border:
-                                Border.all(width: 10, color: Colors.black38)),
-                        child:
-                            Expanded(child: Image.asset('images/nature.jpg')))
-                  ],
-                ))));
+            body: ListView(children: [
+              for (int i = 0; i <= 100; i++)
+                Slidable(
+                    key: ValueKey(i),
+                    startActionPane: ActionPane(
+                        motion: const ScrollMotion(),
+                        dismissible: DismissiblePane(onDismissed: () {}),
+                        children: const [
+                          SlidableAction(
+                              onPressed: null,
+                              icon: Icons.edit,
+                              backgroundColor: Colors.blue,
+                              label: 'Edit',
+                              foregroundColor: Colors.white),
+                          SlidableAction(
+                              onPressed: null,
+                              icon: Icons.delete,
+                              backgroundColor: Colors.red,
+                              label: 'Delete',
+                              foregroundColor: Colors.white),
+                        ]),
+                    child: Container(
+                        decoration: BoxDecoration(border: Border.all()),
+                        child: list(
+                            'Ngabirano', 'Archange', '25', 'UB', Icons.edit)))
+            ])));
+  }
+
+  ListTile list(
+      String first, String last, String age, String school, IconData icon) {
+    return ListTile(
+      title: Column(children: [
+        Row(children: [
+          Text(first,
+              style:
+                  const TextStyle(fontSize: 12, fontStyle: FontStyle.italic)),
+          const SizedBox(width: 30),
+          Text(last,
+              style:
+                  const TextStyle(fontSize: 12, fontStyle: FontStyle.italic)),
+        ]),
+        const SizedBox(height: 5),
+        Row(children: [
+          Text(age,
+              style:
+                  const TextStyle(fontSize: 12, fontStyle: FontStyle.italic)),
+          Container(
+              margin: const EdgeInsets.only(left: 200),
+              child: Row(children: [
+                IconButton(
+                    icon: const Icon(Icons.edit, color: Colors.blue),
+                    onPressed: () {},
+                    tooltip: 'Edit'),
+                IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () {},
+                    tooltip: 'Delete')
+              ]))
+        ]),
+        const SizedBox(height: 5),
+        Row(children: [
+          Text(school,
+              style:
+                  const TextStyle(fontSize: 12, fontStyle: FontStyle.italic)),
+        ]),
+      ]),
+      leading: const CircleAvatar(
+          backgroundImage: AssetImage('images/nature1.jpg'), radius: 30),
+    );
   }
 }
